fd968ec8bec47bcc2a5cbc59e776bd909dedf13c
DanielSalmeron3b <danielesajr@gmail.com>
2022-10-25 15:13:30 -0600

    Added the Edit functionality. Adding own CSS later

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,9 @@
-from venv import create
 from django.urls import path
-from .views import delete_task, list_tasks, create_task
+from .views import delete_task, list_tasks, create_task, edit_task
 
 urlpatterns = [
-    path('', list_tasks),
+    path('', list_tasks, name='tasks'),
     path('new_task/', create_task, name='create_task'),
     path('delete_task/<int:id>/', delete_task, name='delete_task'),
+    path('edit_task/<int:id>/', edit_task, name='edit_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 
@@ -19,3 +20,11 @@ def delete_task(request, id):
     deleted_task = Task.objects.get(id=id)
     deleted_task.delete()
     return redirect('/tasks/')
+
+def edit_task(request, id):
+    task = Task.objects.get(id=id)
+    forms = TaskForm(request.POST or None, instance=task)
+    if forms.is_valid() and request.POST:
+        forms.save()
+        return  redirect('/tasks/')
+    return render(request, 'edit_task.html', {'forms': forms})
